26da988abc37e753b1dffd742721ebb756b90b8f
Shryler <105290336+Shryler@users.noreply.github.com>
2022-09-25 19:02:16 +0200

    breadcrumbs, filter, category, productDetail

--- controllers/product.controller.php
+++ controllers/product.controller.php
@@ -2,5 +2,14 @@
 {
     public function affectDataToRow(&$row, $sub_rows)
     {
+        // Category_product
+        if (isset($sub_rows['category'])) {
+            $categorys = array_values(array_filter($sub_rows['category'], function ($item) use ($row) {
+                return $item->Id_product == $row->Id_product;
+            }));
+            if (isset($categorys)) {  // Récupère plusieurs objets du tableau product (getAll)
+                $row->category_list = count($categorys) == 1 ? array_shift($categorys) : null;
+            }
+        }
     }
 }
